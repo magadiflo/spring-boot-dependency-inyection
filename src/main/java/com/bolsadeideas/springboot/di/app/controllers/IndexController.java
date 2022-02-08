631a22e5048687f2639b598cf7219c9d319a3766
Martín <magadiflo@gmail.com>
2022-02-08 18:25:40 -0500

    Anotación @Qualifier

--- src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.di.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,8 @@ import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 @Controller
 public class IndexController {
 	
-	@Autowired //Para inyectar un objeto que está registrado en el contenedor de Spring
+	@Autowired //Para inyectar un objeto que está registrado en el contenedor de Spring. Como hay dos clases concretas que implmentan la interfaz, inyectará el que tenga la anotación @Primary
+	//@Qualifier("miServicioComplejo")//Para inyectar una clase concreta que tenga como nombre de componente "miServicioComplejo (independientemente si tiene la anotación @Primary)"
 	private IServicio servicio;
 	
 	@GetMapping({"", "/", "/index"})

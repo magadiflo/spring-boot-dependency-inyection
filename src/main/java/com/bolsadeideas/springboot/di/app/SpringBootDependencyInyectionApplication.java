8fca93eb8207da441f3f694fca558c08326eb436
Martín <magadiflo@gmail.com>
2022-02-07 19:30:58 -0500

    Creando nuevo proyecto y controlador

---
+++ src/main/java/com/bolsadeideas/springboot/di/app/SpringBootDependencyInyectionApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.di.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDependencyInyectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDependencyInyectionApplication.class, args);
+	}
+
+}

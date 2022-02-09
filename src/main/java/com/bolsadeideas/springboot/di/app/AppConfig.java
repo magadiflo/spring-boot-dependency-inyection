dfccc22e65f2cfcc1df058d644011f7d1659605c
Martín <magadiflo@gmail.com>
2022-02-08 23:01:23 -0500

    Registrando otras líneas para la factura, dos opciones: @Primary o @Qualifier, aquí se dejó en @Primary

--- src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
@@ -32,15 +32,31 @@ public class AppConfig {
 
 	@Bean("itemsFactura")
 	public List<ItemFactura> registrarItems() {
-		Producto p1 = new Producto("Monitor LG", 770);
-		Producto p2 = new Producto("Teclado Micronics", 67);
-		Producto p3 = new Producto("Bocina Pc", 77);
+		Producto p1 = new Producto("Cámara Sony", 650);
+		Producto p2 = new Producto("Bicicleta Bianchi Aro 26", 890);
+		Producto p3 = new Producto("Luces LED direcionales", 15);
 
 		ItemFactura item1 = new ItemFactura(p1, 27);
 		ItemFactura item2 = new ItemFactura(p2, 73);
 		ItemFactura item3 = new ItemFactura(p3, 67);
 
 		return Arrays.asList(item1, item2, item3);
+	}
+	
+	@Bean("itemsFacturaOficina")
+	@Primary
+	public List<ItemFactura> registrarItemsOficina() {
+		Producto p1 = new Producto("Monitor LG 27'", 750);
+		Producto p2 = new Producto("Teclado Micronics", 80);
+		Producto p3 = new Producto("Bocina Pc", 77);
+		Producto p4 = new Producto("Impresora Epson L350 multifuncional", 730);
+		
+		ItemFactura item1 = new ItemFactura(p1, 2);
+		ItemFactura item2 = new ItemFactura(p2, 1);
+		ItemFactura item3 = new ItemFactura(p3, 1);
+		ItemFactura item4 = new ItemFactura(p4, 1);
+		
+		return Arrays.asList(item1, item2, item3, item4);
 	}
 
 }

9a591feba16e1b2cb285e928211d693f053f2d0a
Martín <magadiflo@gmail.com>
2022-02-08 19:29:55 -0500

    Ejemplo Factura y relaciones entre sus componentes

---
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/domain/ItemFactura.java
@@ -0,0 +1,35 @@
+package com.bolsadeideas.springboot.di.app.models.domain;
+
+public class ItemFactura {
+
+	private Producto producto;
+	private Integer cantidad;
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("ItemFactura [producto=");
+		builder.append(producto);
+		builder.append(", cantidad=");
+		builder.append(cantidad);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
---
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Producto.java
@@ -0,0 +1,35 @@
+package com.bolsadeideas.springboot.di.app.models.domain;
+
+public class Producto {
+
+	private String nombre;
+	private Integer precio;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Integer precio) {
+		this.precio = precio;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Producto [nombre=");
+		builder.append(nombre);
+		builder.append(", precio=");
+		builder.append(precio);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}

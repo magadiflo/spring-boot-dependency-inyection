bdc79e1ee4fba39309d361e17522f5c6b3bc0d62
Martín <magadiflo@gmail.com>
2022-02-08 23:10:51 -0500

    Ciclo de vida del componente

--- src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
@@ -2,6 +2,9 @@ package com.bolsadeideas.springboot.di.app.models.domain;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -17,6 +20,22 @@ public class Factura {
 
 	@Autowired //Se está inyectando del AppConfig.java, método registrarItems()
 	private List<ItemFactura> items;
+	
+	/**
+	 * Se ejecuta justo después de creado el objeto y después de
+	 * haber inyectado las dependencias en los atributos.
+	 */
+	@PostConstruct
+	public  void inicializar() {
+		//Por ejemplo, usamos el postConstruct para cambiar valores a los atributos
+		this.cliente.setNombre(this.cliente.getNombre().concat(" ").concat("Gaspar"));
+		this.descripcion = this.descripcion.concat(" del cliente: ").concat(this.cliente.getNombre());
+	}
+	
+	@PreDestroy
+	public void destruir() {
+		System.out.println("Factura destruida: ".concat(this.descripcion));
+	}
 
 	public String getDescripcion() {
 		return descripcion;
